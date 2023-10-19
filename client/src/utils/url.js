420f0b5e756270339cbd5ad0c3780e5fabac9406
Anurag <anuragyadav41682@gmail.com>
2023-10-19 05:53:59 +0530

    Updated URL

--- client/src/utils/url.js
+++ client/src/utils/url.js
@@ -1,3 +1,4 @@
 // const URL = "http://localhost:3001";
-const URL = "https://jobtracker.hop.sh";
+// const URL = "https://jobtracker.hop.sh";
+const URL = "https://job-tracker-backend-hazel.vercel.app";
 export default URL;
