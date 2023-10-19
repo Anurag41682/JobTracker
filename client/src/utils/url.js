32dfae9330ea9760b47bdb5c4f37162a45f5d9c6
Anurag <anuragyadav41682@gmail.com>
2023-10-19 16:52:06 +0530

    Updated URL

--- client/src/utils/url.js
+++ client/src/utils/url.js
@@ -1,4 +1,5 @@
 // const URL = "http://localhost:3001";
-const URL =
-  "http://anur4g.cgekbwd5g5aubwb0.centralindia.azurecontainer.io:3001";
+// const URL =
+//   "http://anur4g.cgekbwd5g5aubwb0.centralindia.azurecontainer.io:3001";
+const URL = "https://job-tracker-backend-4d82.onrender.com";
 export default URL;
